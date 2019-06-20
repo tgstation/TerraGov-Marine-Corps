cdd7aa1f055139f49f79c2a0cbdd7f6df3460aa1
PsyKzz <matt.daemon660@gmail.com>
2019-06-20 01:29:34 +0100

    equal brackets

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -36,7 +36,7 @@ if [ "$BUILD_TOOLS" = false ]; then
 		echo "mixed <tab><space> indentation detected"
 		exit 1
 	fi
-	if grep '^\/[\w\/].+(var\/|, ?var\/.*).*\)$' $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH); then
+	if grep '^\/[\w\/].+\(.*(var\/|, ?var\/.*).*\)$' $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH); then
 		echo "changed files contains proc argument starting with 'var'"
 		exit 1
 	fi
