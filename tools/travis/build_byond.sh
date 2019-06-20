c54511dfa5409c600ca13d44461a072b9b7dbc9b
PsyKzz <matt.daemon660@gmail.com>
2019-06-20 01:18:51 +0100

    remove non-capture groups

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -36,11 +36,10 @@ if [ "$BUILD_TOOLS" = false ]; then
 		echo "mixed <tab><space> indentation detected"
 		exit 1
 	fi
-	if grep '^(?:\/\w+)+\((?:.*, ?var\/.*|var\/.*)\)' $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH); then
+	if grep '^\/[\w\/].+(var\/|, ?var\/.*).*\)$' $(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH); then
 		echo "changed files contains proc argument starting with 'var'"
 		exit 1
 	fi
-
 
 	(! grep 'step_[xy]' _maps/**/*.dmm)
 	source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
