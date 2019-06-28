922c36b1d53b8573e7b3ebaf02bcb3519527127c
Rohesie <rohesie@gmail.com>
2019-06-28 11:40:51 -0300

    Revert Lintenfixes (#2130)

--- tools/travis/check_filedirs.sh
+++ tools/travis/check_filedirs.sh
@@ -12,33 +12,3 @@ then
     echo "File DIR was ticked, please untick it, see: https://tgstation13.org/phpBB/viewtopic.php?f=5&t=321 for more"
     exit 1
 fi
-
-compile=0
-cd ~
-if [ -d "SpacemanDMM" ]; then
-    cd SpacemanDMM
-    git fetch
-    if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-        compile=1
-        git pull
-    fi
-else
-    git clone https://github.com/SpaceManiac/SpacemanDMM.git
-    cd SpacemanDMM
-    compile=1
-fi
-
-if [ "$compile" == 1 ]; then 
-    while true
-    do
-        echo "heartbeat"
-        sleep 60
-    done &
-
-    cargo build --verbose -p dreamchecker --release
-
-    kill %1
-fi
-
-cd ~/build/tgstation/TerraGov-Marine-Corps
-~/SpacemanDMM/target/release/dreamchecker
