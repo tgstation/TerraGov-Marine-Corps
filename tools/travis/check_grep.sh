6be20fb6cf7dc7dc215c5be217d0d03b9fd4d417
spookydonut <github@spooksoftware.com>
2019-09-20 00:32:42 +0800

    fugg off cable check

--- tools/travis/check_grep.sh
+++ tools/travis/check_grep.sh
@@ -21,10 +21,6 @@ fi;
 if grep 'pixel_[xy] = 0' _maps/**/*.dmm;	then
     echo "pixel_x/pixel_y = 0 variables detected in maps, please review to ensure they are not dirty varedits."
 fi;
-if grep -P '\td[1-2] =' _maps/**/*.dmm;	then
-    echo "d1/d2 cable variables detected in maps, please remove them."
-    st=1
-fi;
 if grep '^/area/.+[\{]' _maps/**/*.dmm;	then
     echo "Vareditted /area path use detected in maps, please replace with proper paths."
     st=1
