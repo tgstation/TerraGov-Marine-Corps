992565bb02e8591bad8a15bf57b6bd7e3aeb6ff8
spookydonut <github@spooksoftware.com>
2019-10-26 19:21:49 +0800

    build

--- tgui-next/packages/tgui/interfaces/Vending.js
+++ tgui-next/packages/tgui/interfaces/Vending.js
@@ -14,8 +14,8 @@ export const Vending = props => {
           title={"You have selected "+data.currently_vending_name}
           buttons={
             <Button
-            onClick={() => act(ref, 'cancel_buying')}
-            icon="times">
+              onClick={() => act(ref, 'cancel_buying')}
+              icon="times">
               Cancel
             </Button>
           }>
@@ -30,47 +30,45 @@ export const Vending = props => {
           </Box>
         </Section>
       ) : (
-        (!!((data.premium_length > 0) || (data.isshared > 0)))
-          &&
-            <Section
-              title={"Coin slot: "+(data.coin ? data.coin : "No coin inserted")}
-              buttons={data.coin && (
+        (!!((data.premium_length > 0) || (data.isshared > 0))) && (
+          <Section
+            title={"Coin slot: "+(data.coin ? data.coin : "No coin inserted")}
+            buttons={data.coin && (
               <Button
                 icon="donate"
-                onClick={() => act(ref, 'remove_coin')}>
+                onClick={() => act(ref, "remove_coin")}>
                 Remove
-              </Button>)
-              }>
-              {!!data.coin && (
+              </Button>)}>
+            {!!data.coin && (
               <Section title="Select an item">
                 {data.coin_records.map(coin_record => (
                   <Box key={coin_record.id}>
-                  <Button
-                    selected={data.currently_vending_index === coin_record.prod_index}
-                    onClick={() => act(ref, 'vend', {vend: coin_record.prod_index, cat: coin_record.prod_cat})}
-                    disabled={!coin_record.amount}>
-                    <Box color={coin_record.product_color} bold={1}>
-                      { decodeHtmlEntities(coin_record.product_name)}
-                    </Box>
-                  </Button>
-                </Box>
+                    <Button
+                      selected={data.currently_vending_index === coin_record.prod_index}
+                      onClick={() => act(ref, "vend", {vend: coin_record.prod_index, cat: coin_record.prod_cat})}
+                      disabled={!coin_record.amount}>
+                      <Box color={coin_record.product_color} bold={1}>
+                        { decodeHtmlEntities(coin_record.product_name)}
+                      </Box>
+                    </Button>
+                  </Box>
                 ))}
               </Section>)}
-            </Section>
-      )}
+          </Section>
+        ))}
       {data.hidden_records.length > 0 && (
         <Section title="$*FD!!F">
           {data.hidden_records.map(hidden_record => (
             <Box key={hidden_record.id}>
-            <Button
-              selected={data.currently_vending_index === hidden_record.prod_index}
-              onClick={() => act(ref, 'vend', {vend: hidden_record.prod_index, cat: hidden_record.prod_cat})}
-              disabled={!hidden_record.amount}>
-              <Box color={hidden_record.product_color} bold={1}>
-                { decodeHtmlEntities(hidden_record.product_name)}
-              </Box>
-            </Button>
-          </Box>
+              <Button
+                selected={data.currently_vending_index === hidden_record.prod_index}
+                onClick={() => act(ref, 'vend', {vend: hidden_record.prod_index, cat: hidden_record.prod_cat})}
+                disabled={!hidden_record.amount}>
+                <Box color={hidden_record.product_color} bold={1}>
+                  { decodeHtmlEntities(hidden_record.product_name)}
+                </Box>
+              </Button>
+            </Box>
           ))}
         </Section>
       )}
