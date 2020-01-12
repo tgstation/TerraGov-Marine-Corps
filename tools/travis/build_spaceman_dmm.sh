13df644d21593d3060ee13f7e074f3d325a33235
spookydonut <github@spooksoftware.com>
2020-01-12 23:25:05 +0800

    aaaaa

--- tools/travis/build_spaceman_dmm.sh
+++ tools/travis/build_spaceman_dmm.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 source dependencies.sh
 
+cd ~
 git clone --depth 1 https://github.com/SpaceManiac/SpacemanDMM.git
 cd SpacemanDMM
 cargo build --bin $1 --release && cp target/release/$1 ~/$1
-cd ~
 
 chmod +x ~/$1
 ~/$1 --version
