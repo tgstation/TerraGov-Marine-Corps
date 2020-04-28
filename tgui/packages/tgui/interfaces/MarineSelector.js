f55bca1a50d46cb56be4fa681b1ab9ae9e50db36
spookydonut <github@spooksoftware.com>
2020-04-28 18:29:02 +0800

    cleanup

--- tgui/packages/tgui/interfaces/MarineSelector.js
+++ tgui/packages/tgui/interfaces/MarineSelector.js
@@ -1,6 +1,5 @@
 import { useBackend, useLocalState } from '../backend';
 import { Button, Section, Box, ProgressBar, LabeledList, Modal } from '../components';
-import { map } from 'common/collections';
 import { Window } from '../layouts';
 import { Fragment } from 'inferno';
 import { LabeledListItem } from '../components/LabeledList';
@@ -19,13 +18,24 @@ export const MarineSelector = (props, context) => {
   ] = useLocalState(context, 'showDesc', null);
 
   const {
-    displayed_records,
     current_m_points,
     total_marine_points,
-    cats,
-    vendor_name,
     show_points,
   } = data;
+
+  const categories = Object
+    .keys(data.displayed_records)
+    .map(key => ({
+      ...data.cats[key],
+      name: key,
+      entries: data.displayed_records[key],
+    }))
+    .filter(category => (
+      category.entries.length > 0
+      && (showEmpty
+      || category.remaining > 0
+      || !category.total && !!current_m_points)
+    ));
 
   return (
     <Window>
@@ -69,21 +79,11 @@ export const MarineSelector = (props, context) => {
         <div className="VendingWindow__content">
           <Window.Content scrollable>
             <Section>
-              {map((entry, category_name) => {
-                const {
-                  remaining,
-                  total,
-                } = cats[category_name];
-                return (
-                  entry.length > 0
-                  && (showEmpty || (remaining > 0
-                    || (!total && !!current_m_points))) && (
-                    <ItemCategory
-                      category_name={category_name}
-                      entries={entry}
-                      key={category_name} />
-                  ));
-              })(displayed_records)}
+              {categories.map(category => (
+                <ItemCategory
+                  category={category}
+                  key={category.id} />
+              ))}
             </Section>
           </Window.Content>
         </div>
@@ -93,25 +93,20 @@ export const MarineSelector = (props, context) => {
 };
 
 const ItemCategory = (props, context) => {
-  const { act, data } = useBackend(context);
-
-  const { category_name, entries } = props;
-
   const {
-    remaining,
-    total,
-  } = data.cats[category_name];
-
-  const [
-    showDesc,
-    setShowDesc,
-  ] = useLocalState(context, 'showDesc', null);
+    category: {
+      entries,
+      name,
+      remaining,
+      total,
+    },
+  } = props;
 
   const cant_buy = total && !remaining;
 
   return (
     <Section
-      title={category_name}
+      title={name}
       buttons={!!total && (
         <ProgressBar
           value={remaining
@@ -125,53 +120,77 @@ const ItemCategory = (props, context) => {
         </ProgressBar>)}>
       <LabeledList>
         {entries.map(display_record => {
-          const {
-            id,
-            prod_cost,
-            prod_index,
-            prod_color,
-            prod_name,
-            prod_desc,
-          } = display_record;
           return (
-            <LabeledListItem
-              key={id}
-              buttons={
-                <Fragment>
-                  {prod_color === "white"
-                  && (<Box inline mr={1} ml={1}>Essential!</Box>)}
-                  {prod_color === "orange"
-                  && (
-                    <Box inline mr={1} ml={1} color="orange">
-                      Recommended
-                    </Box>
-                  )}
-                  {prod_cost > 0
-                  && (
-                    <Box inline width="75px" mr={1} ml={1}>
-                      {prod_cost} points
-                    </Box>
-                  )}
-                  <Button
-                    disabled={cant_buy || prod_cost > data.current_m_points}
-                    onClick={() => act(
-                      'vend',
-                      { vend: prod_index })}
-                    selected={prod_color === "white"}>
-                    Vend
-                  </Button>
-                </Fragment>
-              }
-              label={prod_name}
-              labelColor="white">
-              {!!prod_desc && (
-                <Button onClick={() => setShowDesc(prod_desc)}>
-                  ?
-                </Button>)}
-            </LabeledListItem>
+            <ItemLine
+              display_record={display_record}
+              key={display_record.id}
+              cant_buy={cant_buy} />
           ); }
         )}
       </LabeledList>
     </Section>
+  );
+};
+
+const ItemLine = (props, context) => {
+  const { act, data } = useBackend(context);
+
+  const [
+    showDesc,
+    setShowDesc,
+  ] = useLocalState(context, 'showDesc', null);
+
+  const {
+    current_m_points,
+  } = data;
+
+  const {
+    display_record: {
+      id,
+      prod_cost,
+      prod_index,
+      prod_color,
+      prod_name,
+      prod_desc,
+    },
+    cant_buy,
+  } = props;
+
+  return (
+    <LabeledListItem
+      key={id}
+      buttons={
+        <Fragment>
+          {prod_color === "white"
+          && (<Box inline mr={1} ml={1}>Essential!</Box>)}
+          {prod_color === "orange"
+          && (
+            <Box inline mr={1} ml={1} color="orange">
+              Recommended
+            </Box>
+          )}
+          {prod_cost > 0
+          && (
+            <Box inline width="75px" mr={1} ml={1}>
+              {prod_cost} points
+            </Box>
+          )}
+          <Button
+            disabled={cant_buy || prod_cost > current_m_points}
+            onClick={() => act(
+              'vend',
+              { vend: prod_index })}
+            selected={prod_color === "white"}>
+            Vend
+          </Button>
+        </Fragment>
+      }
+      label={prod_name}
+      labelColor="white">
+      {!!prod_desc && (
+        <Button onClick={() => setShowDesc(prod_desc)}>
+          ?
+        </Button>)}
+    </LabeledListItem>
   );
 };
