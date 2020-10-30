bdb8e0bbae9cc79252dfab318d578a2187946160
spookydonut <github@spooksoftware.com>
2020-10-30 12:49:45 +0800

    more fix

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -9,6 +9,7 @@ mkdir -p ci_test/config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
+ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
 DreamDaemon tgmc.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
