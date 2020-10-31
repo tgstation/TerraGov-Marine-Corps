91d2d4c657a3164d34daca8be5c50351c39a6f66
spookydonut <github@spooksoftware.com>
2020-11-01 01:31:36 +0800

    debug

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -11,6 +11,9 @@ cp config/maps.txt ci_test/config/maps.txt
 cp config/shipmaps.txt ci_test/config/maps.txt
 
 cd ci_test
+ls -l
+ls -l config/
+cat config/*.txt
 ln -s $HOME/libmariadb/libmariadb.so libmariadb.so
 DreamDaemon tgmc.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
