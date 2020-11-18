313011da7c212d0bf9708c2cb9b6150094bcfb98
TiviPlus <57223640+TiviPlus@users.noreply.github.com>
2020-11-18 19:58:31 +0100

    0 length timer linting

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -55,6 +55,11 @@ if grep -nP '^/*var/' code/**/*.dm; then
     echo "Unmanaged global var use detected in code, please use the helpers."
     st=1
 fi;
+echo "Checking for 0 length timers"
+if grep -nP 'addtimer\(.+, ?0\)($| |/)'code/**/*,dm; then
+	echo "Default timer type with no length detected. Please add the correct flags or use the async macro call"
+	st=1
+fi;
 echo "Checking for space indentation"
 if grep -nP '(^ {2}[^* ])|(^ [^ ])|(^   +)' code/**/*.dm; then
     echo "space indentation detected"
