4743f9c177be7b4e45844ad5a03077157a30bdb0
TiviPlus <57223640+TiviPlus@users.noreply.github.com>
2021-02-13 17:41:44 +0100

    Removes illegal src edits and adds a lint for it

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -55,6 +55,11 @@ if grep -nP '^/*var/' code/**/*.dm; then
     echo "Unmanaged global var use detected in code, please use the helpers."
     st=1
 fi;
+echo "Checking for src changing"
+if grep -nP '\tsrc = null' code/**/*.dmm; then
+    echo "base /turf path use detected in maps, please replace with proper paths."
+    st=1
+fi;
 echo "Checking for 0 length timers"
 if grep -nP 'addtimer\(.+?, ?0\)($| |/)' code/**/*.dm; then
 	echo "Default timer type with no length detected. Please add the correct flags or use the async macro call"
