4d2081f7c91333d01427fb933fcd20723a3c6084
Souchu Bastien <bsouchu@gmail.com>
2021-04-24 15:29:29 +0200

    changed template dm generator

--- tools/ci/template_dm_generator.py
+++ tools/ci/template_dm_generator.py
@@ -3,8 +3,7 @@
 import os
 import sys
 
-folders = ["_maps/RandomRuins", "_maps/RandomZLevels", "_maps/shuttles",
-           "_maps/templates"]
+folders = ["_maps/shuttles", "_maps/templates"]
 
 generated = "_maps/templates.dm"
 
