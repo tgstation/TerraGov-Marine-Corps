9059a21760bb327596f008e5d775762a898b9ba6
Souchu Bastien <bsouchu@gmail.com>
2021-04-24 15:08:10 +0200

    tgstation begone

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -8,6 +8,6 @@ mkdir ci_test/config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
+DreamDaemon tgmc.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
