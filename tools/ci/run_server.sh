d15a9aac113c35a9fd415d31ca7387e55899b0ba
Souchu Bastien <bsouchu@gmail.com>
2021-04-25 15:52:45 +0200

    copy back

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -11,3 +11,5 @@ cd ci_test
 DreamDaemon tgmc.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
+cp config/maps.txt ci_test/config/maps.txt
+cp config/shipmaps.txt ci_test/config/shipmaps.txt
