acbb327e4160d8463ea7963541c860de18bb1bee
Bastien Souchu <bsouchu@gmail.com>
2022-08-02 19:58:58 +0200

    add librust so

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
+pwd
 wget -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
