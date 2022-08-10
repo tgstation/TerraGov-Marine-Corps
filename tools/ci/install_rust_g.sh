21bc4f822ac6398fcacf341dda615465488820fa
Bastien Souchu <bsouchu@gmail.com>
2022-08-10 19:30:31 +0200

    build locally

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 source dependencies.sh
 
+cd ~
+git clone https://github.com/tgstation/rust-g.git
+cd rust-g
+cargo build --release --target i686-unknown-linux-gnu
 mkdir -p ~/.byond/bin
-cp librust_g.so ~/.byond/bin/librust_g.so
+cp target/i686-unknown-linux-gnu/release/librust_g.so ~/.byond/bin
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
