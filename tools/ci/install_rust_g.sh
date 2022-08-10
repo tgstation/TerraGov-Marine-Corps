1334b5025935cd33124db534a44e7ec3505cb907
Bastien Souchu <bsouchu@gmail.com>
2022-08-10 19:56:04 +0200

    g++

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -8,7 +8,7 @@ git clone https://github.com/tgstation/rust-g.git
 cd rust-g
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-get install zlib1g-dev:i386 libssl-dev:i386
+sudo apt-get install g++-multilib zlib1g-dev:i386 libssl-dev:i386
 rustup target add i686-unknown-linux-gnu
 export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --release --target i686-unknown-linux-gnu
