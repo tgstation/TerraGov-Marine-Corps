2857542ef622580734a6ca73388b406e0d7d0009
TiviPlus <572233640+TiviPlus@users.noreply.com>
2022-10-14 09:53:54 +0200

    Revert "Hyperscale Guns (#10953)"
    This reverts commit 2d4f434f3d3ba7cd633871d6e85195713f58c321.

--- tools/hyperscale_converter/converter.py
+++ tools/hyperscale_converter/converter.py
@@ -1,19 +1,13 @@
 from operator import setitem
 import PIL as pillow
 from PIL import Image
-from PIL import PngImagePlugin
-from pathlib import Path
 import os
 
 parentPath = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) #gets the TGMC directory
 path = input("File path of .dmi (Relative to the repository directory): ")
 full_path = os.path.join(parentPath, path).replace('\\', '/')
 try :
-	image = PngImagePlugin.PngImageFile
-	image = PngImagePlugin.PngImageFile(full_path)
-
-	metadata = image.info["Description"]
-	image = image.convert("L")
+	image = Image.open(full_path).convert("L")
 except FileNotFoundError :
 	print("File not found.")
 	exit
@@ -22,7 +16,7 @@ layers = dict()
 for x in range(image.size[0]):
 	for y in range(image.size[1]):
 		pixel = image.getpixel((x,y))
-		if pixel == 192 or pixel == 0 : continue
+		if pixel == 192 : continue
 		newset = list()
 		newset.append((x,y))
 		if pixel in layers.keys() :
@@ -42,10 +36,5 @@ for key in layers.keys() :
 		pixel = pixelList[i]
 		pixelmap[pixel[0], pixel[1]] = 255
 	newpath = (os.path.dirname(__file__) + "\\output\\" + str(key) + ".png").replace('\\', '/')
-	newImage.info.update({"Description":metadata})
-	info = PngImagePlugin.PngInfo()
-	info.add_text("Description", metadata, True)
-	PngImagePlugin.PngImageFile.save(newImage, newpath, save_all=True, pnginfo=info)
-	p = Path(newpath)
-	p.rename(p.with_suffix('.dmi'))
+	newImage.save(newpath)
 print("Successfully split file into " + str(len(layers.keys())) + " sprites.")
