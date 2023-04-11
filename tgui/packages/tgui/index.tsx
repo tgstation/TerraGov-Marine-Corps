175612dec1c581ca5840a0c06169644ed9b0f8af
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2023-04-11 13:00:18 -0400

    Converts core tgui to ts (#74638)
    Converts the remaining core tgui files into typescript
    Builds tests for these files
    Completed some todos to rewrite/refactor functions
    Typescript good
    N/A nothing player facing
    
    ---------
    
    Co-authored-by: Jeremiah <jlsnow301@pm.me>

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -18,13 +18,14 @@ import './styles/themes/syndicate.scss';
 import './styles/themes/som.scss';
 import './styles/themes/xeno.scss';
 
-import { perf } from 'common/perf';
-import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
-import { setupHotKeys } from './hotkeys';
+import { StoreProvider, configureStore } from './store';
+
 import { captureExternalLinks } from './links';
 import { createRenderer } from './renderer';
-import { configureStore, StoreProvider } from './store';
+import { perf } from 'common/perf';
 import { setupGlobalEvents } from './events';
+import { setupHotKeys } from './hotkeys';
+import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
 
 perf.mark('inception', window.performance?.timing?.navigationStart);
 perf.mark('init');
