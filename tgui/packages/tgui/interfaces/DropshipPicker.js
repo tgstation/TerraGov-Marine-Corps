821af5cd8546da3f78799dea1faa79c0d16a0814
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2023-04-25 18:09:29 +0200

    prettierx

--- tgui/packages/tgui/interfaces/DropshipPicker.js
+++ tgui/packages/tgui/interfaces/DropshipPicker.js
@@ -8,28 +8,28 @@ export const DropshipPicker = (_props, context) => {
   return (
     <Window width={620} height={540} title={'Dropship Selector'}>
       <Window.Content>
-		<Flex height={"95%"} direction={"row"}>
-			<Flex.Item width={"40%"}>
-				<ShuttleSelection/>
-			</Flex.Item>
-			<Flex.Item width={"60%"} height={"40%"}>
-				<Box>
-				<img src={data.current_image} height={"284px"}/>
-				</Box>
-				<Section title={"Description - "+ (data.name ? data.name : "None")} fill={true}>
-					<Box>{data.desc !== null ? data.desc : "Pick a ship!"}</Box>
-				</Section>
-			</Flex.Item>
-		</Flex>
-		<Button
-			color={"bad"}
-			width={"100%"}
-			disabled={data.current_ref === null}
-            onClick={() =>
-              act('confirm')
-            }>
-            {"CONFIRM SELECTION"}
-         </Button>
+        <Flex height={'95%'} direction={'row'}>
+          <Flex.Item width={'40%'}>
+            <ShuttleSelection />
+          </Flex.Item>
+          <Flex.Item width={'60%'} height={'40%'}>
+            <Box>
+              <img src={data.current_image} height={'284px'} />
+            </Box>
+            <Section
+              title={'Description - ' + (data.name ? data.name : 'None')}
+              fill={true}>
+              <Box>{data.desc !== null ? data.desc : 'Pick a ship!'}</Box>
+            </Section>
+          </Flex.Item>
+        </Flex>
+        <Button
+          color={'bad'}
+          width={'100%'}
+          disabled={data.current_ref === null}
+          onClick={() => act('confirm')}>
+          {'CONFIRM SELECTION'}
+        </Button>
       </Window.Content>
     </Window>
   );
@@ -40,17 +40,15 @@ const ShuttleSelection = (props, context) => {
 
   return (
     <Section title="Available Models" fill={true}>
-	{data.shuttles.map((shuttle) => (
-          <Box key={shuttle.ref}>
-            <Button
-			  selected={shuttle.ref === data.current_ref}
-              onClick={() =>
-                act('pickship', { ref: shuttle.ref })
-              }>
-              {shuttle.name}
-            </Button>
-          </Box>
-	))}
+      {data.shuttles.map((shuttle) => (
+        <Box key={shuttle.ref}>
+          <Button
+            selected={shuttle.ref === data.current_ref}
+            onClick={() => act('pickship', { ref: shuttle.ref })}>
+            {shuttle.name}
+          </Button>
+        </Box>
+      ))}
     </Section>
   );
 };
