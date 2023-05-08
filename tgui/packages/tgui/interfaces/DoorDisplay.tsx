c566b95bfef64a60255f212e3363f190cd2354f9
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2023-05-08 15:49:24 +0200

    ok

--- tgui/packages/tgui/interfaces/DoorDisplay.tsx
+++ tgui/packages/tgui/interfaces/DoorDisplay.tsx
@@ -14,30 +14,36 @@ export const DoorDisplay = (props, context) => {
   const { act, data } = useBackend<DoorDisplayData>(context);
   return (
     <Window width={200} height={150} title="Cell Control">
-		<Window.Content>
-		<Section textAlign="center" title={data.id}>
-			<Flex direction="column">
-			<Flex.Item>
-				<Button
-					width="100%"
-					content={data.door_state ? "Close Door" : "Open Door"}
-					color={!data.door_state ? "good" : "bad"}
-					onClick={() => act('door', {state: (!data.door_state)})}
-				/>
-			</Flex.Item>
-			{data.has_shutters ? <>
-			<Flex.Item>
-				<Button
-					width="100%"
-					content={data.shutter_state ? "Close Shutters" : "Open Shutters"}
-					color={!data.shutter_state ? "good" : "bad"}
-					onClick={() => act('shutter', {state: (!data.shutter_state)})}
-				/>
-			</Flex.Item>
-			</> : null}
-			{data.has_flash ? <FlashControl/> : null}
-			</Flex>
-		</Section>
+      <Window.Content>
+        <Section textAlign="center" title={data.id}>
+          <Flex direction="column">
+            <Flex.Item>
+              <Button
+                width="100%"
+                content={data.door_state ? 'Close Door' : 'Open Door'}
+                color={!data.door_state ? 'good' : 'bad'}
+                onClick={() => act('door', { state: !data.door_state })}
+              />
+            </Flex.Item>
+            {data.has_shutters ? (
+              <>
+                <Flex.Item>
+                  <Button
+                    width="100%"
+                    content={
+                      data.shutter_state ? 'Close Shutters' : 'Open Shutters'
+                    }
+                    color={!data.shutter_state ? 'good' : 'bad'}
+                    onClick={() =>
+                      act('shutter', { state: !data.shutter_state })
+                    }
+                  />
+                </Flex.Item>
+              </>
+            ) : null}
+            {data.has_flash ? <FlashControl /> : null}
+          </Flex>
+        </Section>
       </Window.Content>
     </Window>
   );
@@ -48,15 +54,12 @@ const FlashControl = (props, context) => {
   const { ship_status, fuel_left, fuel_max } = data;
   return (
     <>
-        <Box textAlign="center">
-          <Button
-            content="Activate Flash"
-			width="100%"
-			color="average"
-            onClick={() => act('flash')}
-            disabled={false}
-          />
-        </Box>
+      <Button
+        content="Activate Flash"
+        width="100%"
+        color="average"
+        onClick={() => act('flash')}
+      />
     </>
   );
 };
