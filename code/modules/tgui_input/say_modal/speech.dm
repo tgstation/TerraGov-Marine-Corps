HolyTigerGod/Voidcrew

literally just hook asay into tgui say (#73212)
## About The Pull Request

pressing f3 now opens asay in tgui say
## Why It's Good For The Game

![john willard gets
banned](https://user-images.githubusercontent.com/12202230/216750445-addb4f3f-0df4-47da-891d-4ef4820a10ab.png)

also fixes double-escaped special characters caused by #72407 and
finally gets rid of the gross regular tgui input popup
## Changelog
:cl:
fix: asays are no longer double-escaped
admin: asay now uses tgui say
/:cl:

---------

Co-authored-by: Scriptis <scriptis@duck.com>

code/modules/tgui_input/say_modal/speech.dm
@@ -44,6 +44,9 @@
 		if(OOC_CHANNEL)
 			client.ooc(entry)
 			return TRUE
+		if(ADMIN_CHANNEL)
+			client.cmd_admin_say(entry)
+			return TRUE
 	return FALSE
 
 /**

tgstation/TerraGov-Marine-Corps

code/modules/tgui_input/say_modal/speech.dm
@@ -53,6 +53,9 @@
 		if(XOOC_CHANNEL)
 			client.xooc(entry)
 			return TRUE
+		if(ADMIN_CHANNEL)
+			client.asay(entry)
+			return TRUE
 	return FALSE
 
 /**