5e77c71d68da02db7d569cd423acd014497bb14f
TiviPlus <572233640+TiviPlus@users.noreply.com>
2023-09-16 13:01:49 +0200

    More WIP

--- tgui/packages/tgui/interfaces/CampaignMenu/CampaignOverview.tsx
+++ tgui/packages/tgui/interfaces/CampaignMenu/CampaignOverview.tsx
@@ -1,8 +1,17 @@
-import { CampaignData,} from './index';
+import { CampaignData } from './index';
 import { useBackend } from '../../backend';
+import { Section } from '../../components';
 
 export const CampaignOverview = (props, context) => {
   const { act, data } = useBackend<CampaignData>(context);
-  return (
-    );
+  const {
+    current_mission,
+    active_attrition_points,
+    total_attrition_points,
+    faction_leader,
+    victory_points,
+    faction,
+  } = data;
+  const { map_name, objective_description, mission_brief } = current_mission;
+  return <Section title={faction + ' Campaign Overview'}></Section>;
 };
