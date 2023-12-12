059ea939772a59ad3265f4fb66946746e077e8c7
forest <136390975+vvvv-vvvv@users.noreply.github.com>
2023-12-12 22:13:36 +0000

    Fix double-clicking in various UIs
    React has onDoubleClick instead of onDblClick, so let's change

--- tgui/packages/tgui/interfaces/Filteriffic.jsx
+++ tgui/packages/tgui/interfaces/Filteriffic.jsx
@@ -298,7 +298,7 @@ export const Filteriffic = (props) => {
                 />
               </>
             ) : (
-              <Box inline onDblClick={() => setHiddenSecret(true)}>
+              <Box inline onDoubleClick={() => setHiddenSecret(true)}>
                 {name}
               </Box>
             )
--- tgui/packages/tgui/interfaces/ListInputModal.tsx
+++ tgui/packages/tgui/interfaces/ListInputModal.tsx
@@ -195,7 +195,7 @@ const ListDisplay = (props) => {
             id={index}
             key={index}
             onClick={() => onClick(index)}
-            onDblClick={(event) => {
+            onDoubleClick={(event) => {
               event.preventDefault();
               act('submit', { entry: filteredItems[selected] });
             }}
