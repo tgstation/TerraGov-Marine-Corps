dfce052d96ad4a62c5caf75ff5ba681ccd24b057
Lumipharon <amadeusrandom@gmail.com>
2024-06-26 14:58:38 +1200

    basic stuff

--- code/modules/admin/panels/Campaign_panel.dm
+++ code/modules/admin/panels/Campaign_panel.dm
@@ -61,7 +61,7 @@ GLOBAL_DATUM(campaign_admin_panel, /datum/campaign_admin_panel)
 		faction_data["available_missions"] = faction_datum.available_missions
 		faction_data["assets"] = faction_datum.faction_assets
 
-		faction_data_list[faction] = faction_data
+		faction_data_list += list(faction_data)
 	data["faction_data"] = faction_data_list
 
 	return data
@@ -72,11 +72,35 @@ GLOBAL_DATUM(campaign_admin_panel, /datum/campaign_admin_panel)
 		return
 	var/mob/living/user = usr
 	var/datum/game_mode/hvh/campaign/current_mode = SSticker.mode
-	var/datum/faction_stats/faction_datum = current_mode?.stat_list[text2path(params["faction"])]
+	var/datum/faction_stats/faction_datum = current_mode?.stat_list[params["target_faction"]]
 
 	switch(action)
 		if("set_attrition_points")
 			var/combined_attrition = faction_datum.total_attrition_points + faction_datum.active_attrition_points
 			var/choice = tgui_input_number(user, "How much manpower would you like to dedicate to this mission?", "Attrition Point selection", 0, combined_attrition, 0, 60 SECONDS)
 			faction_datum.set_attrition(choice, user)
+			return TRUE
+		if("add_attrition_points")
+			var/choice = tgui_input_number(user, "How much total attrition would you like to add?", "Attrition Point selection", 0, 9999, -9999)
+			faction_datum.total_attrition_points += choice
+			return TRUE
+		if("set_leader")
+			var/choice = tgui_input_list(user, "Who would you like to promote to faction leader?", "Leader selection", GLOB.alive_human_list_faction[faction_datum.faction], null)
+			faction_datum.faction_leader = choice
+			return TRUE
+		if("set_victory_points")
+			var/choice = tgui_input_number(user, "How many victory points would you like to add?", "Attrition Point selection", 0, CAMPAIGN_MAX_VICTORY_POINTS, -CAMPAIGN_MAX_VICTORY_POINTS)
+			faction_datum.victory_points += choice
+			return TRUE
+		if("add_mission")
+			var/choice = tgui_input_list(user, "What mission would you like to add?", "Add mission", subtypesof(/datum/campaign_mission), null)
+			if(!choice)
+				return FALSE
+			faction_datum.add_new_mission(choice)
+			return TRUE
+		if("add_asset")
+			var/choice = tgui_input_list(user, "What asset would you like to add?", "Add asset", subtypesof(/datum/campaign_asset), null)
+			if(!choice)
+				return FALSE
+			faction_datum.add_asset(choice)
 			return TRUE
--- tgui/packages/tgui/interfaces/CampaignAdminPanel.tsx
+++ tgui/packages/tgui/interfaces/CampaignAdminPanel.tsx
@@ -1,9 +1,9 @@
 import { useBackend } from '../backend';
-import { Button, Stack } from '../components';
+import { Button, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
 export type FactionDataList = {
-  faction_data_list: FactionData[];
+  faction_data: FactionData[];
 };
 
 export type FactionData = {
@@ -19,21 +19,82 @@ export type FactionData = {
 export const CampaignAdminPanel = (props) => {
   const { act, data } = useBackend<FactionDataList>();
 
+  if (!data || !data.faction_data) return 'loading...';
   return (
     <Window title={'Campaign Panel'} width={980} height={775}>
       <Window.Content>
-        {data.faction_data_list.map((selected_faction) => (
-          <Stack>
-            <Button icon={'times'} color="red">
-              onClick=
-              {() => {
-                act('set_attrition_points', {
-                  target_faction: selected_faction.faction,
-                });
-              }}
-              set attrition test button
-            </Button>
-          </Stack>
+        {data.faction_data.map((selected_faction) => (
+          <Section key={selected_faction.faction}>
+            <Stack vertical>
+              <Stack.Item>{selected_faction.faction}</Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('set_attrition_points', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Set active Attrition
+                </Button>
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('add_attrition_points', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Add total Attrition
+                </Button>
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('set_leader', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Set faction leader
+                </Button>
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('set_victory_points', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Add Victory Points
+                </Button>
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('add_mission', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Add mission
+                </Button>
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  onClick={() => {
+                    act('add_asset', {
+                      target_faction: selected_faction.faction,
+                    });
+                  }}
+                >
+                  Add Asset
+                </Button>
+              </Stack.Item>
+            </Stack>
+          </Section>
         ))}
       </Window.Content>
     </Window>
