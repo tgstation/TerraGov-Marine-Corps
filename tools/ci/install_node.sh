a0ecffdb9ab63772413f0df291d9cdbc861811a3
TiviPlus <572233640+TiviPlus@users.noreply.com>
2025-02-05 21:14:02 +0100

    Yarn version bump

--- tools/ci/install_node.sh
+++ tools/ci/install_node.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-
 source dependencies.sh
 
 if [[ -e ~/.nvm/nvm.sh ]]; then
 	source ~/.nvm/nvm.sh
-	nvm install $NODE_VERSION
-	nvm use $NODE_VERSION
+	nvm install $NODE_VERSION_LTS
+	nvm use $NODE_VERSION_LTS
 fi
