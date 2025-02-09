9585ff07e3c8e055ba6f4654aa233d9f655a3020
TiviPlus <572233640+TiviPlus@users.noreply.com>
2025-02-09 20:41:49 +0100

    Test commit

--- tgui/packages/tgui/interfaces/Vending.tsx
+++ tgui/packages/tgui/interfaces/Vending.tsx
@@ -1,4 +1,3 @@
-import { useBackend, useLocalState } from '../backend';
 import {
   Box,
   Button,
@@ -9,7 +8,9 @@ import {
   Section,
   Stack,
   Tabs,
-} from '../components';
+} from 'tgui-core/components';
+
+import { useBackend, useLocalState } from '../backend';
 import { Window } from '../layouts';
 
 type VendingData = {
