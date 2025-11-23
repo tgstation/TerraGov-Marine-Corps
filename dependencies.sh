602e9e66640107bd2fe2967d4f44abb40472e214
frog <162057394+forgman6@users.noreply.github.com>
2025-11-22 23:06:46 -0500

    removed dependacies
    bumps spaceman up

--- dependencies.sh
+++ dependencies.sh
@@ -14,8 +14,7 @@ export RUST_G_VERSION=3.11.0
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
-export SPACEMAN_DMM_COMMIT_HASH=3588de97ff09fdd02d96f178392f3c59b19ebfc4
+export SPACEMAN_DMM_VERSION=suite-1.11
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.10.11
