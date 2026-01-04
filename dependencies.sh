92b8f57fd6cc81ed93b76e8619893b5214acecf0
Meghan-Rossi <56671765+Meghan-Rossi@users.noreply.github.com>
2026-01-04 13:16:03 +0000

    Linter update

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,7 @@
 
 # byond version
 export BYOND_MAJOR=516
-export BYOND_MINOR=1659
+export BYOND_MINOR=1675
 
 #rust_g git tag
 export RUST_G_VERSION=3.11.0
@@ -14,8 +14,8 @@ export RUST_G_VERSION=3.11.0
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
-export SPACEMAN_DMM_COMMIT_HASH=3588de97ff09fdd02d96f178392f3c59b19ebfc4
+export SPACEMAN_DMM_VERSION=suite-1.11
+export SPACEMAN_DMM_COMMIT_HASH=0290db5c752fa292f9824521515b603a2afd11dc
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.10.11
